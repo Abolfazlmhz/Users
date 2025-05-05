08e78ee73e4f180b60b7fa3e9b347041f6e3bd3f
Abolfazl <abolfazlmhz8@gmail.com>
2025-05-05 19:42:26 +0330

    fix: middleware and listener

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 import rootReducer from "./rootReducer";
+import { authApi } from "../api/endpoints/authApi";
+import { projectApi } from "../api/endpoints/projectApi";
+import { taskApi } from "../api/endpoints/taskApi";
+import { commentApi } from "../api/endpoints/commentApi";
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    ...rootReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [projectApi.reducerPath]: projectApi.reducer,
+    [taskApi.reducerPath]: taskApi.reducer,
+    [commentApi.reducerPath]: commentApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      projectApi.middleware,
+      taskApi.middleware,
+      commentApi.middleware
+    ),
   devTools: process.env.NODE_ENV !== "production",
 });
+
+setupListeners(store.dispatch);
